6e13f9e83f58d7c6ed4ed757474ee4439dd8217c
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-01-31 23:29:16 +0200

    Improve F# solution to q-1.1

--- chapter 1/q-1.1/fsharp/solution.fsx
+++ chapter 1/q-1.1/fsharp/solution.fsx
@@ -1,11 +1,12 @@
+let isUniqueChars (input: string) =
+    let mutable found = []
+
+    for c in input do
+        if List.contains c found then
+            found <- c :: found
+
+    found.Length = input.Length
+
 
 let input = "Hello World"
-
-let mutable found = []
-
-for c in input do
-    if List.contains c found then found <- c :: found
-
-printfn "%b" (found.Length = input.Length)
-
-
+printfn "%b" (isUniqueChars input)
