f59fc0ed53c9828a6233eb0279618a4da37399e1
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-02-19 22:16:06 +0200

    q-2.1 typescript solution

--- chapter 2/q-2.1/typescript/solution.ts
+++ chapter 2/q-2.1/typescript/solution.ts
@@ -1,7 +1,26 @@
 type ListNode = {
   value: unknown;
-  next: Node | null;
-  previous: Node | null;
+  next: ListNode | null;
 };
 
-function dedupe(head: ListNode): ListNode {}
+function dedupe(head: ListNode): ListNode {
+  let node = head;
+  let next = node.next;
+  const values = new Set([node.value]);
+  while (next) {
+    if (values.has(next.value)) {
+      node.next = next.next;
+      next = node.next;
+    } else {
+      values.add(next.value);
+      node = next;
+      next = node.next;
+    }
+  }
+
+  return head;
+}
+
+const list = { value: 1, next: { value: 2, next: { value: 1, next: null } } };
+const result = dedupe(list);
+console.log(result);
