a3e5bdc80857cedf45daea47afcc12060500c1c3
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-05-27 18:21:15 +0300

    wip: fsharp solution

--- chapter 2/q-2.2/fsharp/solution.fsx
+++ chapter 2/q-2.2/fsharp/solution.fsx
@@ -1,24 +1,47 @@
 open System.Collections.Generic
 
-type LinkedList<'t> =
-    | Node of 't * LinkedList<'t>
-    | End
+type Node = { Value: int; Next: (Node option) ref }
 
-let rec walk =
-    function
-    | End -> printfn "%s" "End"
-    | Node(value, list) ->
-        printfn "%A" value
-        walk list
 
-let rec toLinkedList =
-    function
-    | [] -> End
-    | x :: xs -> Node(x, (toLinkedList xs))
+let rec kthLast list : Node =
+    let mutable kth: Node option ref = ref None
 
-let rec kthLast list k =
+    // Loop through the list and keep track of the kth last element
+    let mutable node: Node option ref = ref list
+    let mutable index = 0
 
-let list = [ 1; 2; 1; 1; 1 ]
-let linkedList = toLinkedList list
-walk linkedList
-let dedupedList = kthLast linkedList 2
+    while (node.Value.IsSome) do
+        if index >= 3 then
+            if kth.Value.IsSome then
+                kth <- kth.Value.Value.Next
+            else
+                kth <- node
+
+        index <- index + 1
+        node <- node.Value.Value.Next
+
+    kth.Value.Value
+
+let list =
+    Some(
+        { Value = 1
+          Next =
+            ref (
+                Some
+                    { Value = 2
+                      Next =
+                        ref (
+                            Some
+                                { Value = 3
+                                  Next =
+                                    ref (
+                                        Some
+                                            { Value = 4
+                                              Next = ref (Some { Value = 5; Next = ref None }) }
+                                    ) }
+                        ) }
+            ) }
+    )
+
+let value = kthLast list
+printfn "%A" value.Value
