b42f48fa911f630b3d4a67ec203483e69d20423b
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2023-10-17 23:19:28 +0300

    q-4.2 typescript solution

--- q-4.2/typescript/solution.ts
+++ q-4.2/typescript/solution.ts
@@ -1,5 +1,28 @@
 type BinaryTreeNode<T> = {
   value: T;
-  left?: BinaryTreeNode<T>;
-  right?: BinaryTreeNode<T>;
+  left?: BinaryTreeNode<T> | null;
+  right?: BinaryTreeNode<T> | null;
 };
+
+function BinaryTree<T>(values: T[]): BinaryTreeNode<T> | null {
+  if (values.length === 0) return null;
+  if (values.length === 1) return { value: values[0] };
+
+  const middle = Math.floor(values.length / 2);
+  const left = values.slice(0, middle);
+  const right = values.slice(middle + 1);
+
+  let root = {
+    value: values[middle],
+    left: BinaryTree(left),
+    right: BinaryTree(right),
+  };
+
+  return root;
+}
+
+const values = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const tree = BinaryTree(values);
+
+console.log(tree);
