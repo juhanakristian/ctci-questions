b1ebdb1778e2c57352dea6685db1918e40646d7e
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2024-09-08 11:04:56 +0300

    5.3. typescript

--- q-5.3/typescript/solution.ts
+++ q-5.3/typescript/solution.ts
@@ -1,1 +1,35 @@
-function isWinner(bits: number, index: number) {}
+function reverse(bits: string) {
+  return bits.split("").reverse().join("");
+}
+
+function count(bits: string) {
+  let i = 0;
+  while (i < bits.length && bits[i] === "1") i++;
+
+  return i;
+}
+
+function seqlen(bits: string, index: number) {
+  const left = reverse(bits.substring(0, index));
+  if (index === bits.length - 1) return count(left) + 1;
+
+  const right = bits.substring(index + 1);
+
+  return count(left) + count(right);
+}
+
+function longestSequence(integer: number) {
+  const bits = integer.toString(2);
+
+  let winner = 0;
+  for (let i = 0; i < bits.length; i++) {
+    if (bits[i] === "0") {
+      const len = seqlen(bits, i) + 1;
+      if (len > winner) winner = len;
+    }
+  }
+
+  return winner;
+}
+
+console.log(longestSequence(1775));
